687d1a300461dea5a200dde43f1985e925297555
israelreyes-gif <israel.reyes@gmail.com>
2026-05-23 01:15:55 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -106,20 +106,16 @@ function checkDayReset() {
 function recordResult(subject, exerciseKey, correct) {
   var s = ST[subject];
   s.hoy++; s.total++;
+  // Contadores acumulados por subtipo (nunca se decrementan)
   if (correct) {
     s.hoyOk++; s.totalOk++;
-    // Guardar aciertos por subtipo para estadísticas de padres
-    s.errors[exerciseKey + '_ok'] = (s.errors[exerciseKey + '_ok'] || 0) + 1;
-    if (s.errors[exerciseKey] && s.errors[exerciseKey] > 0) {
-      s.errors[exerciseKey]--;
-      if (s.errors[exerciseKey] === 0) delete s.errors[exerciseKey];
-    }
+    s.errors[exerciseKey + '_ok']   = (s.errors[exerciseKey + '_ok']   || 0) + 1;
     if (subject === 'mates') ST.matesStreak++;
     if (subject === 'lengua') ST.gramStreak++;
   } else {
-    s.errors[exerciseKey] = (s.errors[exerciseKey] || 0) + 1;
+    s.errors[exerciseKey + '_fail'] = (s.errors[exerciseKey + '_fail'] || 0) + 1;
     if (subject === 'mates') ST.matesStreak = Math.max(0, ST.matesStreak - 1);
-    if (subject === 'lengua') ST.gramStreak = Math.max(0, ST.gramStreak - 1);
+    if (subject === 'lengua') ST.gramStreak  = Math.max(0, ST.gramStreak  - 1);
   }
   saveState();
 }
