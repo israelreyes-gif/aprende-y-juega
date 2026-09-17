2a4b0e835e577b556f6557865c0c4ec7846374c9
israelreyes-gif <israel.reyes@gmail.com>
2026-09-17 22:34:25 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -126,14 +126,11 @@ function loadScreenLazy(screenId, callback) {
     's-english-study-c4':          'screens/curso4-english.html',
     's-english-exercises-c4':      'screens/curso4-english.html',
     's-english-tobe-c4':           'screens/curso4-english.html',
-    's-english-tobe-present-c4':   'screens/curso4-english.html',
-    's-english-tobe-past-c4':      'screens/curso4-english.html',
-    's-english-simple-c4':         'screens/curso4-english.html',
     's-english-simple-present-c4': 'screens/curso4-english.html',
     's-english-simple-past-c4':    'screens/curso4-english.html',
-    's-english-ing-c4':            'screens/curso4-english.html',
     's-english-ing-present-c4':    'screens/curso4-english.html',
-    's-english-ing-past-c4':       'screens/curso4-english.html'
+    's-english-ing-past-c4':       'screens/curso4-english.html',
+    's-english-future-c4':         'screens/curso4-english.html'
   };
 
   var file = fileMap[screenId];
