23f35dc9a8bf229b2534ea9a3a9dc4b20b87403d
israelreyes-gif <israel.reyes@gmail.com>
2026-09-17 22:10:34 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -128,9 +128,9 @@ function loadScreenLazy(screenId, callback) {
     's-english-tobe-c4':           'screens/curso4-english.html',
     's-english-tobe-present-c4':   'screens/curso4-english.html',
     's-english-tobe-past-c4':      'screens/curso4-english.html',
-    's-english-have-c4':           'screens/curso4-english.html',
-    's-english-have-present-c4':   'screens/curso4-english.html',
-    's-english-have-past-c4':      'screens/curso4-english.html',
+    's-english-simple-c4':         'screens/curso4-english.html',
+    's-english-simple-present-c4': 'screens/curso4-english.html',
+    's-english-simple-past-c4':    'screens/curso4-english.html',
     's-english-ing-c4':            'screens/curso4-english.html',
     's-english-ing-present-c4':    'screens/curso4-english.html',
     's-english-ing-past-c4':       'screens/curso4-english.html'
